7c14bf71bf8ff1208299052324fc8199fce60c9a
Tijn Kersjes <tijn@divbyzero.nl>
2016-12-21 18:16:34 +0100

    Final fixes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 const glob = require("glob");
 const path = require("path");
+const fs = require("fs");
 
 const makecb = (resolve, reject) => (err, res) => err ? reject(err) : resolve(res);
-const promisify = fn => (...args) => new Promise((resolve, reject) => fn(...args, makecb(resolve, reject)));
+const wrapPromise = fn => new Promise((resolve, reject) => fn(makecb(resolve, reject)));
 
 const release = (err) => setImmediate(() => { throw err; });
 
@@ -20,31 +21,40 @@ const cleanDeps = (deps, isDirty) =>
 
         // clean sub dependencies
         if (deps[depName].dependencies)
-            result[depName].dependencies = cleanDeps(deps[key].dependencies, isDirty);
+            result[depName].dependencies = cleanDeps(deps[depName].dependencies, isDirty);
 
         return result;
 }, {});
 
-promisify(glob)("./node_modules/**/package.json")
+wrapPromise(cb => glob("./node_modules/**/package.json", cb))
 .then(packages => {
 
     const platformSpecifics = packages.map(pkg => {
         const pkgInfo = requireLocal(pkg);
-        const shouldFilter = typeof pkgInfo.os === "undefined";
+        const shouldFilter = typeof pkgInfo.os !== "undefined";
         return shouldFilter ? pkgInfo.name : null;
     }).filter(x => x !== null);
 
     return platformSpecifics;
 })
 .then(blacklist => {
+
     const isBlacklisted = name => blacklist.includes(name);
     const wrap = requireLocal("npm-shrinkwrap.json");
     const cleanedDeps = cleanDeps(wrap.dependencies, isBlacklisted);
-    return Object.assign({}, wrap, { dependencies: cleanedDeps });
+    wrap.dependencies = cleanedDeps;
+
+    const data = JSON.stringify(cleanedWrap, null, 2);
+    return wrapPromise(cb => fs.writeFile(localPath("npm-shrinkwrap.json"), data, cb))
+    .then(() => {
+        if (blacklist.length > 0) {
+            console.log("Removed " + blacklist.length + " dependencies from shrinkwrap:");
+            console.log(" - " + blacklist.join("\n - ") + "\n");
+        }
+        else {
+            console.log("No dependencies were removed");
+        }
+        process.exit(0);
+    });
 })
-.then(cleanedWrap => {
-    const data = JSON.stringify(cleanedWrap);
-    fs.writeFile(localPath("npm-shrinkwrap.json"), data);
-})
-.then(() => process.exit(0))
 .catch(release);
